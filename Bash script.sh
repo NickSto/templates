703a777c815c744bc8c61653009619cbc041fe54
Nick <nmapsy@gmail.com>
2018-02-23 10:51:25 -0500

    Bash script.sh: Quote ALL the variables!

--- Bash script.sh
+++ Bash script.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-if [ x$BASH = x ] || [ ! $BASH_VERSINFO ] || [ $BASH_VERSINFO -lt 4 ]; then
+if [ "x$BASH" = x ] || [ ! "$BASH_VERSINFO" ] || [ "$BASH_VERSINFO" -lt 4 ]; then
   echo "Error: Must use bash version 4+." >&2
   exit 1
 fi
 set -ue
 
-Usage="Usage: \$ $(basename $0)"
+Usage="Usage: \$ $(basename "$0")"
 
 function main {
-  if [[ $# -lt 1 ]] || [[ $1 == '-h' ]] || [[ $1 == '--help' ]]; then
+  if [[ "$#" -lt 1 ]] || [[ "$1" == '-h' ]] || [[ "$1" == '--help' ]]; then
     fail "$Usage"
   fi
 }
